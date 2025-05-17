5a72b9e2fd33eb40c3f3ca22e08b7f70ae9ff707
kimheungseok <eovnfjfpa@naver.com>
2025-05-17 19:39:35 +0900

    String1_3

---
+++ src/String/StringEx4.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class StringEx4 {
+    public static void main(String[] args) throws IOException {
+        //단어 뒤집기
+        System.setIn(new FileInputStream("src/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0 ; i < n ; i++){
+            solution(sc.next());
+        }
+    }
+    static void solution(String str){
+        char[] arr = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(int i = arr.length-1; i >= 0; i --){
+            sb.append(arr[i]);
+        }
+        System.out.println(sb);
+    }
+}
